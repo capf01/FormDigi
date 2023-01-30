7440f3f163719aa90a6875fb0aff521d5e82bd83
capf01 <cesaraugustopachecoferreira@gmail.com>
2023-01-30 06:46:03 -0400

    login

--- Form/FormDigiBoard/src/main/java/br/com/SistemaLogin/Servlet/LoginControllers.java
+++ Form/FormDigiBoard/src/main/java/br/com/SistemaLogin/Servlet/LoginControllers.java
@@ -1,5 +1,54 @@
 package br.com.SistemaLogin.Servlet;
 
-public class LoginControllers {
+import java.io.IOException;
 
-}
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.SistemaLogin.exception.InvalidUserException;
+
+public class LoginControllers extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+			
+		processRequest(request, response);
+
+	}
+
+
+
+	private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		ValidaDadosRecebidos(req);
+		RequestDispatcher rd = req.getRequestDispatcher("logado.jsp");
+		rd.forward(req, resp);
+	}
+
+
+
+	private void ValidaDadosRecebidos(HttpServletRequest req) throws ServletException {
+		
+		String nomeUsuario = req.getParameter("txtUsuario");
+		String nomeSenha = req.getParameter("txtSenha");
+		
+
+		
+		 if(!nomeUsuario.trim().equals("cesar") || !nomeSenha.trim().equals("cera1234"))
+			throw new InvalidUserException("Login ou Senha inválidos");
+		 
+		 HttpSession session = req.getSession();
+		 session.setAttribute("usuarioautenticado", nomeUsuario);
+		 
+		
+		
+	}
+		
+		
+	}
+
+
---
+++ Form/FormDigiBoard/src/main/java/br/com/SistemaLogin/login/Main.java
@@ -0,0 +1,13 @@
+package br.com.SistemaLogin.login;
+import java.sql.SQLException;
+
+import br.com.SistemaLogin.login.Login;
+import br.com.SistemaLogin.login.LoginDAO;
+public class Main {
+	public static void main(String[] args) throws SQLException{
+		
+		
+		
+		
+	}
+}
